cfe0b89e379c72d467a477fa1d6ea06ed31aba85
Rafael <rafaelniederauer07@gmail.com>
2021-08-15 22:35:58 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Filter} from './components/Filter';
+
+function App() {
+  return (
+    <div className="App">
+      <Filter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Filter.tsx
@@ -0,0 +1,13 @@
+// @flow 
+// digitar fsc
+import * as React from 'react';
+type Props = {
+    
+};
+export const Filter = (props: Props) => {
+    return (
+        <div>
+            <h1>OPA</h1>
+        </div>
+    );
+};
